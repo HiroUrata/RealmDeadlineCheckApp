85f526d4fe73851718821d722852e676d3334c51
HiroUrata <1712026@i.softbank.jp>
2021-10-25 23:02:59 +0900

    RegisterDatas Model 追加

---
+++ RealmDeadlineCheckApp/Model/RegisterDatas.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterDatas.swift
+//  RealmDeadlineCheckApp
+//
+//  Created by UrataHiroki on 2021/10/25.
+//
+
+import RealmSwift
+
+class RegisterDatas:Object{
+    
+    @objc dynamic var productName = String()
+    @objc dynamic var janCode = String()
+    @objc dynamic var deadlineDay = String()
+    @objc dynamic var signUpDay = String()
+}
