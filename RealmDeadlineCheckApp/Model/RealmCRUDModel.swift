aa54f8143db150e7762eebde37db5610884fbdc0
HiroUrata <1712026@i.softbank.jp>
2021-10-26 22:33:24 +0900

    RealmCRUDModel 追加

---
+++ RealmDeadlineCheckApp/Model/RealmCRUDModel.swift
@@ -0,0 +1,14 @@
+//
+//  RealmCRUDModel.swift
+//  RealmDeadlineCheckApp
+//
+//  Created by UrataHiroki on 2021/10/26.
+//
+
+import RealmSwift
+
+class RealmCRUDModel{
+    
+    lazy var readResultAllDatas:[[String:String]] = []
+    
+}
